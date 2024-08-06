d16a46b6529fe65e0c3ea688243badf8331536f8
ayushghetia <55269750+ghetiaayush@users.noreply.github.com>
2024-08-06 22:22:01 +0530

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 import express from 'express';
+import articles from '../data/articles.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', {title: 'Product Listing'});
+  res.render('index', {title: 'Product Listing', articles: articles});
+});
+
+router.get('/articles/:id', (req, res) => {
+  const article = articles.find(a => a.id === parseInt(req.params.id));
+
+  res.render('article', { title: article.name, article });
+});
+
+router.post('/articles', (req, res) => {
+  const { name, body } = req.body;
+
+  const article = {
+    id: Math.floor(Math.random() * 1000000),
+    name,
+    body
+  };
+  articles.push(article);
+  
+  setTimeout(() => {
+    res.render('partials/list', { articles });
+  }, 3000);
 });
 
 export default router;
